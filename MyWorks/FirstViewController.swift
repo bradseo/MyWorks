0ac63387e13b46aa74aadd323d1ce85239acd748
서동현 <donghyun.seo@kbfg.com>
2018-07-23 17:41:50 +0900

    Initial Commit

---
+++ MyWorks/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyWorks
+//
+//  Created by seodonghyun on 2018. 7. 23..
+//  Copyright © 2018년 seodonghyun. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MyWorks/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyWorks
+//
+//  Created by seodonghyun on 2018. 7. 23..
+//  Copyright © 2018년 seodonghyun. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
